fe86d94c4ef0609907fcabaee4c6c3fedd1f87f1
Alex <abistrovs3@gmail.com>
2021-05-16 15:33:35 +0200

    ClassStudentListView

--- AttendanceManagementSystem (Server)/src/view/ClassStudentListViewController.java
+++ AttendanceManagementSystem (Server)/src/view/ClassStudentListViewController.java
@@ -1,6 +1,20 @@
 package view;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class ClassStudentListViewController extends ViewController {
+    @FXML private Label classLabel;
+    @FXML private TextField nameField;
+    @FXML private TextField labelField;
+    @FXML private TableView classStudentTable;
+    @FXML private TableColumn studentNameColumn;
+    @FXML private TableColumn studentIDColumn;
+    @FXML private Label errorLabel;
+
     @Override
     protected void init() {
 
@@ -9,5 +23,17 @@ public class ClassStudentListViewController extends ViewController {
     @Override
     public void reset() {
 
+    }
+
+    @FXML private void addButtonPressed()
+    {
+    }
+
+    @FXML private void backButtonPressed()
+    {
+    }
+
+    @FXML private void removeButtonPressed()
+    {
     }
 }
